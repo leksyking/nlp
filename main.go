2f0ffe3dff27ec835d7123032a70c63ee09cf741
Leksyking <gbemilekeogundipe@gmail.com>
2022-05-21 19:58:37 -0700

    complete slack nlp bot

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/krognol/go-wolfram"
 	"github.com/shomali11/slacker"
+	"github.com/tidwall/gjson"
+	witai "github.com/wit-ai/wit-go/v2"
 )
+
+var wolframClient *wolfram.Client
 
 func PrintCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
@@ -22,17 +28,39 @@ func PrintCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-	if err := godotenv.Load(); err != nil {
+	if err := godotenv.Load(".env"); err != nil {
 		fmt.Println(err)
 	}
 
-	bot := slacker.NewClient(os.Getenv("SLACK_APP_TOKEN"), os.Getenv("SLACK_BOT_TOKEN"))
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	client := witai.NewClient(os.Getenv("SERVER_ACCESS_TOKEN"))
+	wolframClient = &wolfram.Client{AppID: os.Getenv("APPID")}
 
 	go PrintCommandEvents(bot.CommandEvents())
 
-	bot.Command("", &slacker.CommandDefinition{
-		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {},
+	bot.Command("query for bot - <message>", &slacker.CommandDefinition{
+		Description: "Send any question to wolfram",
+		Example:     "Who is the president of Nigeria",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			query := request.Param("message")
+			fmt.Println(query)
+			msg, _ := client.Parse(&witai.MessageRequest{
+				Query: query,
+			})
+			data, _ := json.MarshalIndent(msg, "", "    ")
+			rough := string(data[:])
+			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value")
+			answer := value.String()
+			res, err := wolframClient.GetSpokentAnswerQuery(answer, wolfram.Metric, 1000)
+			if err != nil {
+				fmt.Println("there is an error")
+			}
+			fmt.Println(rough)
+			fmt.Println(value)
+			response.Reply(res)
+		},
 	})
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	if err := bot.Listen(ctx); err != nil {
